561cb4bd3fc755e1456c061b677a1533540d516f
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2021-07-16 16:15:55 +0100

    Added colouring logic and colour buffers for both shapes.

--- cArcade.c
+++ cArcade.c
@@ -76,7 +76,7 @@ bool buildWindow (GLFWwindow **window)
  * @param programID - the program ID which links graphics and shaders to each other and this program.
  * @return boolean - did it work?
  **/
-bool gameLoop(GLFWwindow **window, GLuint *triangleVertexBuffer, GLuint *cubeVertexBuffer, GLuint *programID, mat4 *mvp, GLuint *matrixID)
+bool gameLoop(GLFWwindow **window, GLuint *triangleVertexBuffer, GLuint *triangleColourBuffer, GLuint *cubeVertexBuffer, GLuint *cubeColourBuffer, GLuint *programID, mat4 *mvp, GLuint *matrixID)
 {
   double startTime = glfwGetTime(); /* Start the clock for the FPS Counter */
   int framePerSecondCount = 0; /* Start the FPS counter at 0. */
@@ -102,23 +102,50 @@ bool gameLoop(GLFWwindow **window, GLuint *triangleVertexBuffer, GLuint *cubeVer
       0,                  /* Stride - the byte offset between attributes.  We're packing it in as tight as possible with 0. */
       (void*)0            /* Pointer - specifies the array buffer offset. */
     );
+    
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, *triangleColourBuffer);
+    glVertexAttribPointer(
+      1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+      3,                                // size
+      GL_FLOAT,                         // type
+      GL_FALSE,                         // normalized?
+      0,                                // stride
+      (void*)0                          // array buffer offset
+    );
+
     /* Step 2: draw the data above. */
     glDrawArrays(GL_TRIANGLES, 0, 3); /*Draw triangles, starting at the first point (0), working to the third (3).  One complete triangle. */
     glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
 
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, *cubeVertexBuffer);
     glVertexAttribPointer(
-      0,                  /* Index of the first vertec to be modified. Give each new shape a new one to access it in GLSL.*/
+      0,                  /* Index of the first vertex to be modified.*/
       3,                  /* Size - number of components per vertex attribute. */
       GL_FLOAT,           /* Data type of each component.  We'll use floats. */
       GL_FALSE,           /* Do we need to normalise the values, or treat them as fixed points? */
       0,                  /* Stride - the byte offset between attributes.  We're packing it in as tight as possible with 0. */
       (void*)0            /* Pointer - specifies the array buffer offset. */
     );
+    
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, *cubeColourBuffer);
+    glVertexAttribPointer(
+      1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+      3,                                // size
+      GL_FLOAT,                         // type
+      GL_FALSE,                         // normalized?
+      0,                                // stride
+      (void*)0                          // array buffer offset
+    );
+
     /* Step 2: draw the data above. */
-    glDrawArrays(GL_TRIANGLES, 0, 36); /*Draw triangles, starting at the first point (0), working to the thirty-sixth (36).  One complete cube. */
+    glDrawArrays(GL_TRIANGLES, 3, 36); /*Draw triangles, starting at the first point (0), working to the thirty-sixth (36).  One complete cube. */
+
     glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
     
     /* Step 3: Prepare the next frame of the loop. */
     glfwSwapBuffers(*window);
@@ -225,6 +252,53 @@ int main(void)
       -4.0f, -2.0f, -2.0f,
       -2.0f,-4.0f, -2.0f
   };
+
+  static const GLfloat triangleColourBufferData[] = 
+  {
+    0.583f,  0.771f,  0.014f,
+    0.609f,  0.115f,  0.436f,
+    0.327f,  0.483f,  0.844f
+  };
+
+  static const GLfloat cubeColourBufferData[] = 
+  {
+    0.583f,  0.771f,  0.014f,
+    0.609f,  0.115f,  0.436f,
+    0.327f,  0.483f,  0.844f,
+    0.822f,  0.569f,  0.201f,
+    0.435f,  0.602f,  0.223f,
+    0.310f,  0.747f,  0.185f,
+    0.597f,  0.770f,  0.761f,
+    0.559f,  0.436f,  0.730f,
+    0.359f,  0.583f,  0.152f,
+    0.483f,  0.596f,  0.789f,
+    0.559f,  0.861f,  0.639f,
+    0.195f,  0.548f,  0.859f,
+    0.014f,  0.184f,  0.576f,
+    0.771f,  0.328f,  0.970f,
+    0.406f,  0.615f,  0.116f,
+    0.676f,  0.977f,  0.133f,
+    0.971f,  0.572f,  0.833f,
+    0.140f,  0.616f,  0.489f,
+    0.997f,  0.513f,  0.064f,
+    0.945f,  0.719f,  0.592f,
+    0.543f,  0.021f,  0.978f,
+    0.279f,  0.317f,  0.505f,
+    0.167f,  0.620f,  0.077f,
+    0.347f,  0.857f,  0.137f,
+    0.055f,  0.953f,  0.042f,
+    0.714f,  0.505f,  0.345f,
+    0.783f,  0.290f,  0.734f,
+    0.722f,  0.645f,  0.174f,
+    0.302f,  0.455f,  0.848f,
+    0.225f,  0.587f,  0.040f,
+    0.517f,  0.713f,  0.338f,
+    0.053f,  0.959f,  0.120f,
+    0.393f,  0.621f,  0.362f,
+    0.673f,  0.211f,  0.457f,
+    0.820f,  0.883f,  0.371f,
+    0.982f,  0.099f,  0.879f
+};
   
   GLuint triangleVertexBuffer;
   glGenBuffers(1, &triangleVertexBuffer); /* Generate a buffer for triangleVertexBuffer. */
@@ -232,11 +306,21 @@ int main(void)
   glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertexBufferData), triangleVertexBufferData, GL_STATIC_DRAW); /* Give our vertices to OpenGL. */
 
   GLuint cubeVertexBuffer;
-  glGenBuffers(1, &cubeVertexBuffer); /* Generate a buffer for triangleVertexBuffer. */
+  glGenBuffers(1, &cubeVertexBuffer); /* Generate a buffer for cubeVertexBuffer. */
   glBindBuffer(GL_ARRAY_BUFFER, cubeVertexBuffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertexBufferData), cubeVertexBufferData, GL_STATIC_DRAW); /* Give our vertices to OpenGL. */
 
-  if (!gameLoop(&window, &triangleVertexBuffer, &cubeVertexBuffer, &programID, &mvp, &matrixID))
+  GLuint triangleColourBuffer;
+  glGenBuffers(1, &triangleColourBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, triangleColourBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(triangleColourBufferData), triangleColourBufferData, GL_STATIC_DRAW);
+
+  GLuint cubeColourBuffer;
+  glGenBuffers(1, &cubeColourBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, cubeColourBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeColourBufferData), cubeColourBufferData, GL_STATIC_DRAW);
+
+  if (!gameLoop(&window, &triangleVertexBuffer, &triangleColourBuffer, &cubeVertexBuffer, &cubeColourBuffer, &programID, &mvp, &matrixID))
     return EXIT_FAILURE;
 
   glfwDestroyWindow(window);
