b41d31afa8f018e8ef289477ae368e4234cc3bef
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2021-07-16 16:16:39 +0100

    Shaders now handle colour passed from the code.

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -1,9 +1,13 @@
 #version 460 core
 
+in vec3 fragmentColor;
+
 // Ouput data
 out vec3 color;
 
 void main()
 {
-  color = vec3(1,0,0);
+  //color = vec3(1,0,0);
+
+  color = fragmentColor;
 }
--- vertexShader.glsl
+++ vertexShader.glsl
@@ -2,6 +2,10 @@
 
 // Input vertex data, different for all executions of this shader.
 layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec3 vertexColor;
+
+out vec3 fragmentColor;
+
 uniform mat4 MVP;
 
 void main()
@@ -9,4 +13,6 @@ void main()
 //  gl_Position.xyz = vertexPosition_modelspace;
 //  gl_Position.w = 1.0;
   gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
+
+  fragmentColor = vertexColor;
 }
